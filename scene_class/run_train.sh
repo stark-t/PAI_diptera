81e7ad0503645cf0cd2533872f2ee9785275ac61
star_th <thomas.stark@dlr.de>
2024-02-19 11:48:13 +0100

    run train predict after each other

--- scene_class/run_train.sh
+++ scene_class/run_train.sh
@@ -1,7 +1,7 @@
-# nohup python /mnt/ushelf_star_th/projects/2023_PAI/2023_PAI_diptera/PAI_diptera/scene_class/scene_class_order.py \
-# --config /mnt/ushelf_star_th/projects/2023_PAI/2023_PAI_diptera/PAI_diptera/scene_class/config.yaml 
-# > /mnt/ushelf_star_th/projects/2023_PAI/2023_PAI_diptera/PAI_diptera/scene_class/efficient_seed99.out
-
+nohup python /mnt/ushelf_star_th/projects/2023_PAI/2023_PAI_diptera/PAI_diptera/scene_class/scene_class_order.py \
+--config /mnt/ushelf_star_th/projects/2023_PAI/2023_PAI_diptera/PAI_diptera/scene_class/config.yaml 
+> /mnt/ushelf_star_th/projects/2023_PAI/2023_PAI_diptera/PAI_diptera/logs/nohups/efficient_train_seed1337.out \
+\
 nohup python /mnt/ushelf_star_th/projects/2023_PAI/2023_PAI_diptera/PAI_diptera/scene_class/run_predict.py \
 --config /mnt/ushelf_star_th/projects/2023_PAI/2023_PAI_diptera/PAI_diptera/scene_class/config.yaml 
-> /mnt/ushelf_star_th/projects/2023_PAI/2023_PAI_diptera/PAI_diptera/scene_class/efficient_seed99_predict.out
+> /mnt/ushelf_star_th/projects/2023_PAI/2023_PAI_diptera/PAI_diptera/logs/nohups/efficient_predict_seed1337.out
